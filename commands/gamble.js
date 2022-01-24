3fa580b64baedead8e1ef19af25b03e2e10290db
RealizmModz <74314445+RealismModz@users.noreply.github.com>
2022-01-24 07:53:44 +0000

    More economy crap

--- commands/gamble.js
+++ commands/gamble.js
@@ -7,8 +7,8 @@ function format(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 module.exports = {
-	name: 'pay',
-	description: 'Pay other users',
+	name: 'gamble',
+	description: 'Gamble your money away',
 	aliases:[],
 	botPermissions: [
 		"SEND_MESSAGES",
@@ -18,10 +18,27 @@ module.exports = {
 	permissions: [
 		"SEND_MESSAGES",
 	],
-	cooldown: 2,
+	cooldown: 0,
 	async execute(client, message, args, Discord, cmd) {
 		let user = message.mentions.members.first()
-		let money = args[1]
-		
+		let money = args[0]
+      if (isNaN(money)) return message.reply("Amount is not a number.");
+      let result = await cs.gamble({
+        user: message.author.id,
+        guild: message.guild.id,
+        amount: money,
+        minAmount: 1,
+        cooldown: 25 
+      });
+      if (result.error) {
+        if (result.type == 'amount') return message.reply("Please insert an amount first.");
+        if (result.type == 'nan') return message.reply("The amount was not a number.");
+        if (result.type == 'low-money') return message.reply(`You don't have enough money. You need $${format(result.neededMoney)} more to perform the action. `);
+        if (result.type == 'gamble-limit') return message.reply(`You don't have enough money for gambling. The minimum was $${result.minAmount}.`);
+        if (result.type == 'time') return message.reply(`Wooo that is too fast. You need to wait **${result.second}** second(s) before you can gamble again.`);
+      } else {
+        if (result.type == 'lost') return message.reply(`Ahh, no. You lose $${result.amount}. You have $${result.wallet} left. Good luck next time.`);
+        if (result.type == 'won') return message.reply(`Woohoo! You won $${result.amount}! You have $${result.wallet}. Good luck, have fun!`);
+      }
 	}
 }
