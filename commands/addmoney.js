8a2cbe3247c85d0cf30c37c282f47a62e5956ac5
RealizmModz <74314445+RealismModz@users.noreply.github.com>
2022-01-24 03:38:40 +0000

    Update User Schema

---
+++ commands/addmoney.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: 'addmoney',
+	description: 'Gives money to a user',
+	aliases:['am','gm','givemoney','givem','moneygive','mg'],
+	botPermissions: [
+		"SEND_MESSAGES",
+		"VIEW_CHANNEL",
+		"READ_MESSAGE_HISTORY"
+	],
+	permissions:[
+		"ADMINISTRATOR"
+	],
+	cooldown: 1,
+	async execute(client, message, args, Discord, cmd) {
+		const User = require('../schemas/userSchema');
+
+		console.log(User.findOne({ userId: message.author.id }));
+	}
+}
